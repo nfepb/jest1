d6ed6b61bf4c648960a00d3d065befc1dab6aba9
nfepb <nicolas.planteb@gmail.com>
2022-09-01 17:28:08 +0000

    Initial commit with Jest npm

---
+++ scripts/tests/calc.test.js
@@ -0,0 +1,13 @@
+// const { default: test } = require("node:test");
+const addition = require("../calc");
+
+describe("Calculator", () => {
+  describe("Addition function", () => {
+    test("should return 42 for 20 +22", () => {
+      expect(addition(20, 22)).toBe(42);
+    });
+  });
+  describe("Substraction function", () => {});
+  describe("Multiplication function", () => {});
+  describe("Division function", () => {});
+});
